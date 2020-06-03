d7d17828ac0abeb49a78ff5b500312665a221817
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-06-03 13:59:40 +0700

    Create konfig.php

---
+++ config/konfig.php
@@ -0,0 +1,29 @@
+<?php
+///SILAHKAN MASUKAN DATA YANG DIBUTUHKAN///
+///GUNAKAN SCRIPT INI DENGAN BIJAK///
+
+///Headers Ke 1 Anda///
+$ua = array(
+"Host:---",
+"authorization:---",
+"content-language: in",
+"luid:---",
+"tk:---",
+"tuid:---",
+"accept-encoding: gzip",
+"user-agent:---");
+
+///HEADERS KE 2///
+$head = array(
+"Host:---",
+"content-type:---",
+"user-agent:---",
+"content-language: in",
+"x-requested-with: com.match.winner",
+"accept-encoding: gzip, deflate",
+"accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7");
+
+///Data Claim///
+
+$dt = "--------";
+?>
