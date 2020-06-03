7494d98e4e9941cf8f818d128b880263fb9935ad
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-06-03 14:06:03 +0700

    Create config.php

---
+++ config/cc/config.php
@@ -0,0 +1,28 @@
+<?php
+
+$ua = array(
+"Authentication: ---",
+"token: ---",
+"deviceid: ---",
+"Host: api.coinclub.global",
+"Connection: Keep-Alive",
+"Accept-Encoding: gzip",
+"User-Agent: ---");
+
+$login = "-----";
+
+
+$head = array(
+"Authentication: ---",
+"token: ---",
+"deviceid: ---",
+"Content-Type: application/x-www-form-urlencoded;charset=UTF-8",
+"Host: api.coinclub.global",
+"Connection: Keep-Alive",
+"Accept-Encoding: gzip",
+"User-Agent: ---");
+
+
+
+$cnf = "------";
+?>
